98772a12ad42896fa971a1eecc396c38b75dc0eb
Donavan Aziaka <donavan.aziaka@epitech.eu>
2017-07-21 17:53:23 +0200

    update camera and multiple ressources such as a girl with animations

--- Unlit/Classes/HelloWorldScene.h
+++ Unlit/Classes/HelloWorldScene.h
@@ -6,23 +6,35 @@
 class HelloWorld : public cocos2d::Scene
 {
  private: // variables
-  cocos2d::Sprite3D* sprite;
-  cocos2d::Vec3 spritePos;
+  cocos2d::Sprite3D* player;
+  cocos2d::Vec3 playerPos;
   cocos2d::Vec3 spriteDest;
+  cocos2d::Vec2 distance;
   cocos2d::Camera* camera;
   cocos2d::Vec2 clickPos;
+  cocos2d::Label* labelTouch;
  public:
   // main feature to create scene
   static cocos2d::Scene* createScene();
   virtual bool init() override;
 
 
-  // functions that will
+  // functions that will init datas
   void setupCamera();
   void setupCharacter();
-  void getClickPosition();
+  void setupTerrain();
+  void setupTouchEvent();
   void menuCloseCallback(cocos2d::Ref* pSender);
 
+
+  // touch events
+  virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+  virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+  virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+  virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
+
+
+  // refresh
   void update(float) override;
 
   CREATE_FUNC(HelloWorld);
